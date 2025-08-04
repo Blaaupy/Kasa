7ad0e9f9d8969d9ebb9b700e26ec6a72989ecba6
Baptiste <baptiste.laurent0027@gmail.com>
2025-08-04 04:21:00 +0200

    NotFoundPage Done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <h1>Page d’accueil</h1>;
+import { Link } from "react-router-dom";
+import "./NotFound.scss"
+
+export default function NotFoundPage() {
+  return (
+  <>
+    <p className="p404">404</p>
+    <h1>Oups! La page que vous demandez n'existe pas.</h1>
+    <div className="linkToHomePage">
+      <Link to="/">Retourner sur la page d'Accueil</Link>
+    </div>
+    
+  </>);
 }
