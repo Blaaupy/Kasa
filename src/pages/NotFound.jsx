223d7f1913e97af01adc21ed386acc046e36b4a0
Baptiste <baptiste.laurent0027@gmail.com>
2025-08-12 15:03:00 +0200

    Version téléphone NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,12 +3,13 @@ import "./NotFound.scss"
 
 export default function NotFoundPage() {
   return (
-  <>
-    <p className="p404">404</p>
+  <div className="container div404">
+    
+      <p className="p404">404</p>
+    
     <h1 className="notFoundTitle">Oups! La page que vous demandez n'existe pas.</h1>
     <div className="linkToHomePage">
       <Link to="/">Retourner sur la page d'Accueil</Link>
     </div>
-    
-  </>);
+  </div>);
 }
